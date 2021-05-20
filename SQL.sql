f8bef4789c79b7c30ae8ccfe9ff78f8e2cea3a9f
Matt <drennanmattheww@gmail.com>
2021-05-20 16:14:11 -0400

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -3,13 +3,19 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 28, 2021 at 02:36 PM
+-- Generation Time: May 20, 2021 at 08:13 PM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `troop`
@@ -85,6 +91,7 @@ CREATE TABLE IF NOT EXISTS `costumes` (
 DROP TABLE IF EXISTS `events`;
 CREATE TABLE IF NOT EXISTS `events` (
   `id` int(100) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `oldid` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `venue` varchar(255) DEFAULT NULL,
   `dateStart` datetime DEFAULT NULL,
@@ -179,6 +186,7 @@ CREATE TABLE IF NOT EXISTS `notifications` (
 DROP TABLE IF EXISTS `troopers`;
 CREATE TABLE IF NOT EXISTS `troopers` (
   `id` int(100) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `oldid` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(240) DEFAULT NULL,
   `phone` varchar(10) DEFAULT NULL,
@@ -194,3 +202,7 @@ CREATE TABLE IF NOT EXISTS `troopers` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
