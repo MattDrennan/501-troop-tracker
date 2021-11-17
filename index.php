16f483bcf3beb867e320d0e958ec5f95d58b407f
Matt <drennanmattheww@gmail.com>
2021-11-17 14:44:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1429,7 +1429,7 @@ if(isset($_GET['action']) && $_GET['action'] == "commandstaff")
 						</td>
 						
 						<td>
-							'.$db->name.'
+							<a href="index.php?profile='.$db->id.'">'.$db->name.'</a>
 						</td>
 						
 						<td>
