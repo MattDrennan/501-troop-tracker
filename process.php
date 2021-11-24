b3b0b70d5454d46c9eddf1379b7530ca13dc98a9
Matt <drennanmattheww@gmail.com>
2021-11-23 21:06:21 -0500

    Update process.php

--- process.php
+++ process.php
@@ -317,14 +317,18 @@ if(isset($_GET['do']) && $_GET['do'] == "postcomment" && isset($_POST['submitCom
 			}
 		}
 		
-		// Notify e-mail for comments
-		$query = "SELECT events.id AS eventId, events.name AS eventName, event_sign_up.id AS signupId, troopers.id AS trooperId, troopers.email AS email, troopers.name AS name FROM events LEFT JOIN event_sign_up ON events.id = event_sign_up.troopid LEFT JOIN troopers ON event_sign_up.trooperid = troopers.id WHERE event_sign_up.troopid = '".cleanInput($_POST['eventId'])."' AND troopers.subscribe = '1' AND troopers.email != '' GROUP BY troopers.id";
-		if ($result = mysqli_query($conn, $query))
+		// Check comment check
+		if($commentCheck->num_rows == 0)
 		{
-			while ($db = mysqli_fetch_object($result))
+			// Notify e-mail for comments
+			$query = "SELECT events.id AS eventId, events.name AS eventName, event_sign_up.id AS signupId, troopers.id AS trooperId, troopers.email AS email, troopers.name AS name FROM events LEFT JOIN event_sign_up ON events.id = event_sign_up.troopid LEFT JOIN troopers ON event_sign_up.trooperid = troopers.id WHERE event_sign_up.troopid = '".cleanInput($_POST['eventId'])."' AND troopers.subscribe = '1' AND troopers.email != '' GROUP BY troopers.id";
+			if ($result = mysqli_query($conn, $query))
 			{
-				// Send E-mail
-				sendEmail($db->email, $db->name, "Troop Tracker: A comment has posted on ".$db->eventName."!", getName(cleanInput($_SESSION['id'])) . ": " . cleanInput($_POST['comment']) . "\n\nYou can opt out of e-mails under: \"Manage Account\"\n\nhttps://trooptracking.com");
+				while ($db = mysqli_fetch_object($result))
+				{
+					// Send E-mail
+					sendEmail($db->email, $db->name, "Troop Tracker: A comment has posted on ".$db->eventName."!", getName(cleanInput($_SESSION['id'])) . ": " . cleanInput($_POST['comment']) . "\n\nYou can opt out of e-mails under: \"Manage Account\"\n\nhttps://trooptracking.com");
+				}
 			}
 		}
 
