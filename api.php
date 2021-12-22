63012e4b2feb66a7d1af81a540db5fcb2d3c02a9
Matthew Drennan <64912127+MattDrennan@users.noreply.github.com>
2021-12-22 06:48:33 -0500

    Update api.php

--- api.php
+++ api.php
@@ -4,7 +4,7 @@
 include "config.php";
 
 // Query - Suppress
-@$query = "SELECT event_sign_up.trooperid, event_sign_up.troopid, event_sign_up.costume, event_sign_up.status, event_sign_up.attended_costume, events.name AS eventName, events.id AS eventId, events.moneyRaised, events.dateStart, events.dateEnd, troopers.id, troopers.name, troopers.forum_id, troopers.rebelforum, troopers.tkid, troopers.squad FROM events LEFT JOIN event_sign_up ON events.id = event_sign_up.troopid JOIN troopers ON troopers.id = event_sign_up.trooperid WHERE (troopers.id = '".cleanInput($_GET['trooperid'])."' OR (troopers.tkid = '".cleanInput($_GET['tkid'])."') AND troopers.squad = '".cleanInput($_GET['squad'])."') AND events.closed = '1' AND event_sign_up.status = '3' ORDER BY events.dateEnd DESC";
+@$query = "SELECT event_sign_up.trooperid, event_sign_up.troopid, event_sign_up.costume, event_sign_up.status, events.name AS eventName, events.id AS eventId, events.moneyRaised, events.dateStart, events.dateEnd, troopers.id, troopers.name, troopers.forum_id, troopers.rebelforum, troopers.tkid, troopers.squad FROM events LEFT JOIN event_sign_up ON events.id = event_sign_up.troopid JOIN troopers ON troopers.id = event_sign_up.trooperid WHERE (troopers.id = '".cleanInput($_GET['trooperid'])."' OR (troopers.tkid = '".cleanInput($_GET['tkid'])."') AND troopers.squad = '".cleanInput($_GET['squad'])."') AND events.closed = '1' AND event_sign_up.status = '3' ORDER BY events.dateEnd DESC";
 
 // Start count
 $i = 0;
@@ -31,7 +31,7 @@ if ($result = mysqli_query($conn, $query) or die($conn->error))
 		$trooperRebelForum = $db->rebelforum;
 		
 		// Start eventWithin
-		$eventWithin = array("eventID" => $db->id, "eventName" => $db->eventName, "dateStart" => $db->dateStart, "dateEnd" => $db->dateEnd);
+		$eventWithin = array("eventID" => $db->eventId, "eventName" => $db->eventName, "dateStart" => $db->dateStart, "dateEnd" => $db->dateEnd);
 		
 		// Push events
 		array_push($eventArray, $eventWithin);
