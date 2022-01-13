04e93ee319cc58fa2615da13a900b2e654237c0e
Matthew Drennan <64912127+MattDrennan@users.noreply.github.com>
2022-01-13 12:39:50 -0500

    Update config.php

--- config.php
+++ config.php
@@ -3065,14 +3065,11 @@ function eventClubCount($eventID, $club)
 						$other++;
 						$total++;
 					}
-					// All
+					// DUAL
 					else if($db2->club == 5)
 					{
 						$i++;
 						$rl++;
-						$mandos++;
-						$droidb++;
-						$other++;
 						$total++;
 					}							
 				}
