0609e95d703abbbf49082280083c663fb86bd70d
Matthew Drennan <64912127+MattDrennan@users.noreply.github.com>
2022-03-26 21:40:02 -0400

    Saber Guild Cron update

--- script/php/cron/sgsync.php
+++ script/php/cron/sgsync.php
@@ -18,62 +18,11 @@ include(dirname(__DIR__) . '/../../tool/dom/simple_html_dom.php');
 // Purge SG troopers
 $conn->query("DELETE FROM sg_troopers") or die($conn->error);
 
-/*
-// get DOM from URL or file
-$html = file_get_html('https://saberguild.org/member-gallery/');
-
-// Loop through comments
-foreach($html->find('div.single-team-area') as $a)
-{
-	foreach($a->find('a') as $b)
-	{
-		// Get title which contains ID and name
-		$title = $b->title;
-		$title = explode("&#8211;", $title);
-
-		// Translate to different variables
-		$id = trim($title[0]);
-		$id = preg_replace('/[^0-9]/', '', $id);
-
-		$name = trim($title[1]);
-
-		$link = $b->href;
-
-		$image = "";
-
-		foreach($a->find('img') as $b)
-		{
-			// Set image
-			$image = $b->src;
-
-			// Print
-			print_r($b->src);
-			echo '<br />';
-			break;
-		}
-
-		// Insert into database
-		$conn->query("INSERT INTO sg_troopers (sgid, name, image, link) VALUES ('".$id."', '".$name."', '".$image."', '".$link."')") or die($conn->error);
-
-		// Print
-		print_r($id);
-		echo '<br />';
-		print_r($name);
-		echo '<br />';
-		print_r($link);
-		echo '<br />';
-		break;
-	}
-
-	echo '<hr />';
-}
-*/
-
 // Pull extra data from spreadsheet
 $values = getSheet("1PcveycMujakkKeG2m4y8iFunrFbo2KVpQJ00GyPI3b8", "Sheet1");
 
 // Reset Saber Guild Status
-$conn->query("UPDATE troopers SET pOther = 0 WHERE sgid > 0") or die($conn->error);
+$conn->query("UPDATE troopers SET pSG = 0 WHERE sgid > 0") or die($conn->error);
 
 // Set up count
 $i = 0;
@@ -97,7 +46,7 @@ foreach($values as $value)
 		$conn->query("INSERT INTO sg_troopers (sgid, name, image, ranktitle, costumename, link) VALUES ('".cleanInput($value[2])."', '".cleanInput($value[0])."', '".cleanInput($image)."', '".cleanInput($value[1])."', '".cleanInput($value[3])."', '')") or die($conn->error);
 		
 		// Update status to regular member
-		$conn->query("UPDATE troopers SET pOther = 1 WHERE sgid = '".str_replace("SG-", "", $value[2])."'") or die($conn->error);
+		$conn->query("UPDATE troopers SET pSG = 1 WHERE sgid = '".str_replace("SG-", "", $value[2])."'") or die($conn->error);
 	}
 
 	// Increment
