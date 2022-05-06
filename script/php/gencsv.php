3bb4cee5e32695ccef575cbc387d1c9fd8569609
Matthew Drennan <64912127+MattDrennan@users.noreply.github.com>
2022-05-06 16:57:19 -0400

    Update gencsv.php

--- script/php/gencsv.php
+++ script/php/gencsv.php
@@ -22,8 +22,8 @@ if ($result = mysqli_query($conn, $query))
 {
     while ($db = mysqli_fetch_object($result))
     {
-        array_push($list, ['', $db->name, '']);
-        array_push($list, ['Trooper Name', 'TKID', 'Forum Name', 'Status']);
+        array_push($list, ['', '', $db->name, '']);
+        array_push($list, ['Trooper Name', 'TKID', 'Forum Name', 'Costume', 'Status']);
 
         // Query database for roster info
         $query2 = "SELECT event_sign_up.id AS signId, event_sign_up.note, event_sign_up.costume_backup, event_sign_up.costume, event_sign_up.status, event_sign_up.troopid, event_sign_up.addedby, event_sign_up.status, event_sign_up.signuptime, troopers.id AS trooperId, troopers.name, troopers.tkid, troopers.forum_id, troopers.squad FROM event_sign_up JOIN troopers ON troopers.id = event_sign_up.trooperid WHERE troopid = '".cleanInput($_GET['troopid'])."' ORDER BY event_sign_up.id ASC";
@@ -32,7 +32,7 @@ if ($result = mysqli_query($conn, $query))
         {
             while ($db2 = mysqli_fetch_object($result2))
             {
-                array_push($list, [$db2->name, $db2->tkid, $db2->forum_id, getStatus($db2->status)]);
+                array_push($list, [$db2->name, $db2->tkid, $db2->forum_id, getCostume($db2->costume), getStatus($db2->status)]);
             }
         }
     }
@@ -47,8 +47,6 @@ function array_to_csv_download($array, $filename = "export.csv", $delimiter=",")
     header('Content-Type: application/csv');
     header('Content-Disposition: attachment; filename="'.$filename.'";');
 
-    // open the "output" stream
-    // see http://www.php.net/manual/en/wrappers.php.php#refsect2-wrappers.php-unknown-unknown-unknown-descriptioq
     $f = fopen('php://output', 'w');
 
     foreach ($array as $line) {
